aeac6c5889f80fc729f5f00353f2dc510ec20b0f
snowman <unmem@outlook.com>
2020-12-16 13:41:07 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 
-ORIG_HEAD="$(git name-rev --name-only HEAD)"
+if [ "`git status -s`" ]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
 
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public origin/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
 hugo
 
-git show-ref refs/heads/github-pages && git branch -D github-pages
-git checkout --orphan github-pages
-git rm -r --cached .
-mv docs/* .
-rmdir docs
-git commit -am "Build doc"
-git push
-git checkout "$ORIG_HEAD"
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages"
+
+echo "Pushing to github"
+git push --all
